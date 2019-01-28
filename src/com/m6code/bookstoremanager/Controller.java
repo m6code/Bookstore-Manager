31285879777d545746e5d94159618cbca08e436d
M6code <benjamynfrank101@gmail.com>
2019-01-28 13:58:27 +0100

    initial commit for bookstore manager

---
+++ src/com/m6code/bookstoremanager/Controller.java
@@ -0,0 +1,4 @@
+package com.m6code.bookstoremanager;
+
+public class Controller {
+}
---
+++ src/com/m6code/bookstoremanager/Main.java
@@ -0,0 +1,16 @@
+package com.m6code.bookstoremanager;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
